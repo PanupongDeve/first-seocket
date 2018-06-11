f74b7c44c2f945f88bbadfa8ebf9d7aa4a256bf9
PanupongDeve <5635512110@psu.ac.th>
2018-06-11 17:32:59 +0700

    update

--- SocketController/chatMessage.js
+++ SocketController/chatMessage.js
@@ -8,12 +8,14 @@ const chatMessage1 = (io, socket) => {
 
 const chatMessage2 = (io, socket) => {
     socket.on('chat message2', async (msg) => {
+        const roomId = socket.handshake.query.roomId;
         const data = {
             token: socket.handshake.query.token,
-            msg
+            msg,
+            roomId
         }
         console.log(`Message2:`, data)
-        await io.emit('chat message2', msg);
+        await io.emit(`chat message${roomId}`, msg);
     })
 }
 
